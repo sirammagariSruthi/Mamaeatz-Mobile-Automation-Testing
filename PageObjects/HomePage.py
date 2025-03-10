b2ccd1e72bfd2d19434d82b335a57cdfcf34ff9f
Sruthi Sirammagari <reddysruthi218@gmail.com>
2025-03-10 12:19:44 +0530

    Changes

---
+++ PageObjects/HomePage.py
@@ -0,0 +1,35 @@
+#Home Screen Locators
+
+#Top section
+
+Home_Usericon='//android.view.ViewGroup[@content-desc="Guest User"]/android.widget.ImageView'
+Home_Button_MyOrders='//android.widget.TextView[@text="My Orders"]'
+Home_SearchField='//android.widget.EditText[@text="Search"]'
+
+#Categories in Home Page
+
+Home_Category_Appetizers='//android.view.ViewGroup[@content-desc="Appetizers"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Starters='//android.view.ViewGroup[@content-desc="Starters"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Categoru_Tandoor='//android.view.ViewGroup[@content-desc="Tandoor"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Soup='//android.view.ViewGroup[@content-desc="Soup"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Entrees='//android.view.ViewGroup[@content-desc="Entrees"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_RollsAndLupas='//android.view.ViewGroup[@content-desc="Rolls & Lupas"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_StreetFood='//android.view.ViewGroup[@content-desc="Street Food"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Noodles='//android.view.ViewGroup[@content-desc="Noodles"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_RiceAndBiryani='//android.view.ViewGroup[@content-desc="Rice & Biryani"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_ValueBundles='//android.view.ViewGroup[@content-desc="Value Bundles"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Beverages='//android.view.ViewGroup[@content-desc="Beverages"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Breads='//android.view.ViewGroup[@content-desc="Breads"]/android.view.ViewGroup/android.widget.ImageView'
+Home_Category_Desserts='//android.view.ViewGroup[@content-desc="Desserts"]/android.view.ViewGroup/android.widget.ImageView'
+
+
+#Home Page middle buttons section
+Home_Button_Order='//android.widget.TextView[@text="Order"]'
+Home_Button_Specials='//android.widget.TextView[@text="Specials"]'
+Home_Button_Catering='//android.widget.TextView[@text="Catering"]'
+
+#Bottom navigation Bar
+Home_Tab='//android.widget.Button[@content-desc="Home"]/android.widget.ImageView'
+Menu_Tab='//android.widget.Button[@content-desc="Menu"]/android.widget.ImageView'
+Cart_Tab='//android.widget.Button[@content-desc="1, Cart"]/android.widget.ImageView'
+Profile_Tab='//android.widget.Button[@content-desc="Profile"]/android.widget.ImageView'
